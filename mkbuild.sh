9d5747c09f36fb275992dc5e4dc7a56427a8fd00
José Bollo <jose.bollo@iot.bzh>
2021-01-12 18:43:33 +0100

    Normalize mkbuild.sh

--- mkbuild.sh
+++ mkbuild.sh
@@ -2,10 +2,9 @@
 
 h="$(dirname $0)"
 f=false
-arch=
-bd=
-: ${PREFIX:=}
-eval set -- $(getopt -o b:fp: -l buildir:,force,--prefix: -- "$@") || exit
+: ${bd:=build}
+: ${PREFIX:=$HOME/.local}
+eval set -- $(getopt -o b:fp: -l buildir:,force,prefix: -- "$@") || exit
 while :; do
 	case "$1" in
 	-b|--buildir) bd="$2"; shift;;
@@ -16,14 +15,12 @@ while :; do
 	shift
 done
 
-: ${bd:=build}
-
 mkdir -p "$h/$bd" || exit
 cd "$h/$bd" || exit
 $force && rm -r * 2>/dev/null || rm CMakeCache.txt 2>/dev/null
 
 cmake \
-	-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:=${PREFIX:=$HOME/.local}} \
+	-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:=$PREFIX} \
 	..
 
 make -j "$@"
