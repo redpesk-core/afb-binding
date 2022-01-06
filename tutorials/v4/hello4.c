d82e55e168908f8b3acb8f09e2531deda55080a0
José Bollo <jose.bollo@iot.bzh>
2022-01-06 09:54:45 +0100

    Update copyrights

--- tutorials/v4/hello4.c
+++ tutorials/v4/hello4.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2021 IoT.bzh Company
+ * Copyright (C) 2015-2022 IoT.bzh Company
  * Author: José Bollo <jose.bollo@iot.bzh>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
