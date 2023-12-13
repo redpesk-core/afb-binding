ee38b9ad6009e8188936a80d2417187ecfe25a3f
José Bollo <jose.bollo@iot.bzh>
2023-12-13 10:49:15 +0100

    Add tuto-6 (v3) for permissions

---
+++ tutorials/v3/tuto-6.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015-2023 IoT.bzh Company
+ * Author: José Bollo <jose.bollo@iot.bzh>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-only
+ */
+#define AFB_BINDING_VERSION 3
+#include <afb/afb-binding.h>
+
+void hello(afb_req_t req)
+{
+	AFB_REQ_DEBUG(req, "hello world");
+	afb_req_reply(req, NULL, NULL, "hello world");
+}
+
+static struct afb_auth auths[] = {
+        {
+                .type = afb_auth_Permission,
+                .text = "permission"
+        }
+};
+
+const afb_verb_t verbs[] = {
+	{ .verb="hello", .callback=hello, .auth = &auths[0] },
+	{ .verb=NULL }
+};
+
+const afb_binding_t afbBindingExport = {
+	.api = "tuto-1",
+	.verbs = verbs
+};
+
